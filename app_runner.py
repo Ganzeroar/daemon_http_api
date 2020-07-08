94d5c7890354dddf702a02e9c1bb94f260de9d05
Ganzeroar <ganzeroar@localhost.localdomain>
2020-07-08 17:49:46 +0300

    update runner

--- app_runner.py
+++ app_runner.py
@@ -6,8 +6,9 @@ try:
     os.mkdir(current_path + '/store/')
     os.mkdir(current_path + '/for_functional_tests/')
     os.mkdir(current_path + '/for_unit_tests/')
+    files_db.create_db()
 except:
     pass
-files_db.create_db()
+
 
 os.system('gunicorn -b 127.0.0.1:5000 app:app --daemon')
